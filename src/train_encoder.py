d814c9e3a07fa47fa5526d9ff738a16b781c8c4c
matheus <matheus.abrantesc@gmail.com>
2021-10-28 20:22:16 -0300

    jannitor: removed unnecessary functions
    Signed-off-by: matheus <matheus.abrantesc@gmail.com>

--- src/train_encoder.py
+++ src/train_encoder.py
@@ -4,26 +4,6 @@ import torch
 import click
 
 
-def save_features(feats, output_dir):
-    nfeats = feats.shape[1]
-
-    print(f"max min ", feats.max(), feats.min())
-
-    for i in range(nfeats):
-        tmp = feats[0,i,:,:]
-        print(tmp.shape)
-
-
-        output_img = output_dir + "/" + str(i) + ".png"
-        print("saving images to ", output_img)
-        #plt.imsave(output_img, tmp.numpy(), cmap='gray')
-
-        plt.imshow(tmp, cmap='gray')
-        plt.show()
-
-        break
-
-
 @click.command()
 @click.option('--arch-path', '-a', required=True, type=str, help='Architecture json description file')
 @click.option('--input-dir', '-i', required=True, type=str, help='Input dir. with .png images and .txt markers')
