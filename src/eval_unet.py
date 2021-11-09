5c6ff291145e7cea3423f6bc08f6ab3474647493
matheus <matheus.abrantesc@gmail.com>
2021-11-09 19:00:18 -0300

    added close basins and filtering pos-processing to evaluate script
    Signed-off-by: matheus <matheus.abrantesc@gmail.com>

--- src/eval_unet.py
+++ src/eval_unet.py
@@ -11,6 +11,44 @@ import click
 import cv2
 import os
 from skimage.color import rgb2lab, lab2rgb
+import warnings, traceback
+import inspect
+
+try:
+    import pyift.pyift as ift
+    
+    if ("CreateImageFromNumPy" not in  dir(ift)):
+        raise NotImplementedError("CreateImageFromNumPy not in ift")
+    if ("CloseBasins" not in  dir(ift)):
+        raise NotImplementedError("CloseBasins not in ift")
+    if ("SelectCompAboveArea" not in  dir(ift)):
+        raise NotImplementedError("SelectCompAboveArea not in ift")
+except NotImplementedError as e:
+    print("Warning:", e)
+
+except:
+    ift = None
+    warnings.warn("PyIFT is not installed.")
+
+
+def complete_img_tensor(image, area=2000):
+    out_img = image.clone()
+    if ift is not None:
+        out_img = out_img.detach().cpu().squeeze(0).numpy().astype(np.int32)
+
+        try:
+            iftimage = ift.CreateImageFromNumPy(np.ascontiguousarray(out_img), is3D=False)
+            iftimage = ift.CloseBasins(iftimage, None, None)
+            iftimage = ift.SelectCompAboveArea(iftimage, None, area)
+            out_img  =  iftimage.AsNumPy()
+        except Exception:
+            traceback.print_exc()
+
+        print("close image")
+        out_img = torch.tensor(out_img).unsqueeze(0)
+
+    return out_img
+
 
 def save_output(image, pred_img, output_dir, base_name, inputpath):
 
@@ -36,7 +74,8 @@ def save_output(image, pred_img, output_dir, base_name, inputpath):
 @click.option('--gt_datapath', '-gd',required=True, type=str, help='Path with _label.png images')
 @click.option('--output-dir', '-o', required=False, type=str, help='Output dir, if defined will save the segmentations')
 @click.option('--unet_model', '-u', default='unet.pt', type=str, help='Input .pt unet model (default=unet.pt)')
-def main(arch_path, images_datapath, gt_datapath, unet_model, output_dir):
+@click.option('--min-area', '-ma', default=2000, type=int, help='Minimal area (used only if pyift installed)')
+def main(arch_path, images_datapath, gt_datapath, unet_model, output_dir, min_area):
 
     device = get_device()
     arch = utils.load_architecture(arch_path)
@@ -76,18 +115,19 @@ def main(arch_path, images_datapath, gt_datapath, unet_model, output_dir):
     tmp_iou  = 0
     for bx, data in enumerate(val_dl):
         loss, acc, preds, true_labels, iou = validate_batch(model, (data['img'], data['gt']), criterion, device=device)
+        closed_img = complete_img_tensor(preds, min_area)
 
         if all_preds is None:
-            all_preds = preds.detach().cpu().numpy().flatten()
+            all_preds = closed_img.detach().cpu().numpy().flatten()
             all_true_labels = true_labels.detach().cpu().numpy().flatten()
         else:
-            all_preds = np.concatenate((all_preds, preds.detach().cpu().numpy().flatten()))
+            all_preds = np.concatenate((all_preds, closed_img.detach().cpu().numpy().flatten()))
             all_true_labels = np.concatenate((all_true_labels, true_labels.detach().cpu().numpy().flatten()))
         tmp_loss += loss
         tmp_iou  += iou
 
         if output_dir is not None:
-            tmp_img = preds.detach().cpu().squeeze(0).numpy()
+            tmp_img = closed_img.detach().cpu().squeeze(0).numpy()
             save_output (data['img'], tmp_img, output_dir, data['name'][0], images_datapath)
             
     log_val_loss.append(tmp_loss/(bx+1))
